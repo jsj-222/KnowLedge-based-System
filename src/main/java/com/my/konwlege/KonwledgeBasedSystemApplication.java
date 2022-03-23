69f7a3343aeb1deb34d02d2838e47d0da9351888
wjj <2014232502@qq.com>
2022-03-23 22:11:39 +0800

    2-2 init

---
+++ src/main/java/com/my/konwlege/KonwledgeBasedSystemApplication.java
@@ -0,0 +1,13 @@
+package com.my.konwlege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonwledgeBasedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonwledgeBasedSystemApplication.class, args);
+    }
+
+}
