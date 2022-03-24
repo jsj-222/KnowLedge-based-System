7ce438ae74d66ac1625183d9b422606cb68c5bbd
wjj <2014232502@qq.com>
2022-03-24 16:20:06 +0800

    Revert "3-5 集成mybatis generator 生成test表代码"
    This reverts commit bf63dd6845881454cdaea81f88c91aab1d820c4e.

--- src/main/java/com/my/konwlege/domain/Test.java
+++ src/main/java/com/my/konwlege/domain/Test.java
@@ -1,17 +1,18 @@
 package com.my.konwlege.domain;
 
 public class Test {
-    private Long id;
+
+    private Integer id;
 
     private String name;
 
     private String password;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -29,18 +30,5 @@ public class Test {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", name=").append(name);
-        sb.append(", password=").append(password);
-        sb.append("]");
-        return sb.toString();
     }
 }
--- src/main/java/com/my/konwlege/mapper/TestMapper.java
+++ src/main/java/com/my/konwlege/mapper/TestMapper.java
@@ -1,31 +1,9 @@
 package com.my.konwlege.mapper;
 
 import com.my.konwlege.domain.Test;
-import com.my.konwlege.domain.TestExample;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface TestMapper {
-    long countByExample(TestExample example);
-
-    int deleteByExample(TestExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(Test record);
-
-    int insertSelective(Test record);
-
-    List<Test> selectByExample(TestExample example);
-
-    Test selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
-
-    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
-
-    int updateByPrimaryKeySelective(Test record);
-
-    int updateByPrimaryKey(Test record);
+    public List<Test> list();
 }
