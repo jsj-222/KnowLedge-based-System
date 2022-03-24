0bd052326ea0cb2112a48f107bcdcb2689ad607b
wjj <2014232502@qq.com>
2022-03-24 13:12:08 +0800

    2-8 增加springboot配置文件及一些自定义配置

--- src/main/java/com/my/konwlege/controller/TestController.java
+++ src/main/java/com/my/konwlege/controller/TestController.java
@@ -1,16 +1,23 @@
 package com.my.konwlege.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //返回字符串
 @RestController
 public class TestController {
+    @Value("${test.hello:HE}")
+    private String testHello;
     //接口
     @GetMapping("/hello")
     public String hello(){
-        return "hello World";
+        return "hello World"+testHello;
+    }
+    //接口
+    @PostMapping("/hello/post")
+    public String hello(String name){
+        return "hello World"+name;
     }
 }
