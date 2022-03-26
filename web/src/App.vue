cbd2c314debd8ddab41ea9345a503a8fed93dfe8
wjj <2014232502@qq.com>
2022-03-26 16:54:53 +0800

    5-1 集成axios

--- web/src/App.vue
+++ web/src/App.vue
@@ -15,10 +15,9 @@
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue';
 import TheFooter from '@/components/the-footer.vue';
-// 引用组件
+
 export default defineComponent({
   name: 'app',
-
   components: {
     TheHeader,
     TheFooter,
@@ -26,21 +25,4 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
-</style>
